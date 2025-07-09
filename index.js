1e038d6dce09c87356a8e4d36e94227c9aa265ae
pasindu9922 <pasindunayana68@gmail.com>
2025-07-09 14:14:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Robin Web Pair Bot is Running!'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
